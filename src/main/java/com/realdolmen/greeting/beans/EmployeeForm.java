8ad88e85160275193eeb32622ebf3c14b8517fda
Tjen Dekker <vontjen@gmail.com>
2017-07-04 09:46:00 +0200

    commit from class

--- src/main/java/com/realdolmen/greeting/beans/EmployeeForm.java
+++ src/main/java/com/realdolmen/greeting/beans/EmployeeForm.java
@@ -2,6 +2,7 @@ package com.realdolmen.greeting.beans;
 
 import com.realdolmen.greeting.domain.Employee;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -15,11 +16,23 @@ public class EmployeeForm {
 
     private Employee employee;
 
+
+    @PostConstruct
+    public void init() {
+        employee = new Employee();
+    }
+
     public Employee getEmployee() {
         return employee;
     }
 
     public void setEmployee(Employee employee) {
         this.employee = employee;
+    }
+
+    public String saveEmployee(){
+        System.out.println(employee.toString());
+        return null;
+
     }
 }
